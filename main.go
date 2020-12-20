621e851a3bd49085887a2c4b7a65cb5cd3b64d8a
Bryce-Huang <44901318+Bryce-huang@users.noreply.github.com>
2020-12-20 20:36:58 +0800

    Update main.go

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 	"path/filepath"
 	"regexp"
 	"strconv"
+	"strings"
 )
 
 var POD_NAME = os.Getenv("MY_POD_NAME")
@@ -19,6 +20,8 @@ var NS = os.Getenv("NS")
 
 func main() {
 
+
+
 	var valid = regexp.MustCompile("[0-9]{1,}")
 	id, err := strconv.Atoi(valid.FindString("mariadb-galera-0"))
 	if err != nil {
@@ -26,11 +29,11 @@ func main() {
 	}
 	fmt.Println(id)
 	fmt.Println(filepath.Join("d", "data"))
-	getSeqNum()
+	fmt.Println(getSeqNum())
 
 }
 
-func getSeqNum() (int, error) {
+func getSeqNum() (  int, error) {
 	//	dat, err := os.Open("/bitnami/mariadb/data/grastate.dat")
 	dat, err := os.Open("./grastate.dat")
 	if err != nil {
@@ -38,15 +41,24 @@ func getSeqNum() (int, error) {
 	}
 	defer dat.Close()
 	br := bufio.NewReader(dat)
-	for  {
+	line := 0
+
+	for {
 		s, _, c := br.ReadLine()
+		line++
 		if c == io.EOF {
 			break
 		}
-
-		fmt.Println(string(s))
+		if line != 4 {
+			continue
+		}
+		str:=string(s)
+		num, err := strconv.Atoi(strings.TrimSpace(str[strings.IndexByte(str,':')+1:]))
+		if err != nil {
+			return 0, err
+		}
+		return num,nil
 
 	}
-	return 0,nil
-
+	return 0, nil
 }
